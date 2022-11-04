f7cc6ced6b8fbace182851672acf3eaf9abc1eba
Koko-Mx <116813042+Koko-Mx@users.noreply.github.com>
2022-11-04 12:33:03 +0000

    Added Mr. Lam sample code

--- script.js
+++ script.js
@@ -1,10 +1,64 @@
-
+let phrases = ["fox in a box", "dog at the park", "cat has a hat"];
+let curPhrase;
+let guess;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+	// Make the drawing canvase as big as the window
+	createCanvas(windowWidth, windowHeight);
+	
+	// Set the RGB background colour
+	background(250,250,250);
+	
+	// Set the frame rate
+	frameRate(1);
+	
+	// initiate game
+	selectRandomPhrase();
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
+function selectRandomPhrase() {
+	let index = Math.floor(random(0, phrases.length));
+	print("index is ", index);
+	curPhrase = phrases[index];
+	guess = [];
+	for(let i = 0; i < curPhrase.length; i++) {
+		guess.push(curPhrase[i] == " " ? " " : "_"); 
+		print(i, guess[i]);
+	}
 }
 
 function draw() {
-  noStroke()
+	clear();
+	textSize(50);
+	text(curPhrase, 100, 60);
+	text(guess, 100, 150);
+}
+
+function keyPressed() {
+	// print("key pressed is", key);
+  if (key >= 'a' && key <= 'z') { 
+		print("You guessed", key);
+		
+		// Find all instances of key in curPhrase
+		let result = [];
+		for(var i=0; i < curPhrase.length; i++) {
+    	if (curPhrase[i] === key) {
+				result.push(i);
+				guess[i] = key;
+			}
+		}
+		
+		// Check results for matches
+		if (result.length > 0) {
+			// we found a match
+			print("Found matches at indices", result);
+		}
+		else {
+			print("NO MATCH!");
+		}
+  }
 }
