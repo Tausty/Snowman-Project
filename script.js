0c4b5cbc7bbd5a932f5e81a7f54dbf452b5a6dd8
Taust <116812445+Tausty@users.noreply.github.com>
2022-11-07 14:21:27 +0000

    Added looping of phrases, so the game does not end after the first phrase.

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let curPhrase;
 let guess, wrongguesses = [];
 let whereyouguess = '';
 let amountofsnowmanparts = 0;
-let rightguesses=[];
-let turn=1;
-let p1score=0;
-let p2score=0;
+let rightguesses = [];
+let turn = 1;
+let p1score = 0;
+let p2score = 0;
 let printscore;
-let check=false;
+let check = false;
 
 function setup() {
 	// Make the drawing canvase as big as the window
@@ -35,20 +35,32 @@ function selectRandomPhrase() {
 	for (let i = 0; i < curPhrase.length; i++) {
 		guess.push(curPhrase[i] == " " ? " " : "_");
 		whereyouguess = join(guess, ' ')
+
 		//                               
 	}
 }
 
 function draw() {
 	clear();
-	check=false
+	if (!whereyouguess.includes("_")) {
+		index = Math.floor(random(0, phrases.length));
+		curPhrase = phrases[index];
+		whereyouguess = []
+		guess = []
+		for (let i = 0; i < curPhrase.length; i++) {
+			guess.push(curPhrase[i] == " " ? " " : "_");
+			whereyouguess = join(guess, ' ')
+		}
+
+	}
+	check = false
 	textSize(50);
 	fill(0, 0, 0); // black
 	text(curPhrase, 100, 60);
 	text(whereyouguess, 100, 150);
-	
-	text(p1score,0,50)
-	text(p2score,width-50,50)
+
+	text(p1score, 0, 50)
+	text(p2score, width - 50, 50)
 
 	textSize(30);
 	fill(255, 0, 0); // red
@@ -96,24 +108,28 @@ function keyPressed() {
 		// Find all instances of key in curPhrase
 		let result = [];
 		for (var i = 0; i < curPhrase.length; i++) {
-			
+
 			if (rightguesses.includes(key))
-				check=false
-			
+				check = false
+
 			else if (curPhrase[i] === key) {
-				check=true
-				if (turn==1) {p1score++}
-				if (turn==-1) {p2score++}
-				
+				check = true
+				if (turn == 1) {
+					p1score++
+				}
+				if (turn == -1) {
+					p2score++
+				}
+
 				result.push(i);
 				guess[i] = key;
 				whereyouguess = join(guess, ' ')
 				amountofsnowmanparts++
 			}
-		if (check==true && i==curPhrase.length){
-			turn*=-1
-			append(rightguesses,key);
-		} 
+			if (check == true && i == curPhrase.length) {
+				turn *= -1
+				append(rightguesses, key);
+			}
 		}
 
 		// Check results for matches
@@ -130,9 +146,9 @@ function keyPressed() {
 	}
 }
 
-function lost(){
- 	clear()
+function lost() {
+	clear()
 	textSize(100)
-	fill (255,0,0) //red
-	text("You lost",300,300)
+	fill(255, 0, 0) //red
+	text("You lost", 300, 300)
 }
